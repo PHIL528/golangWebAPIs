f208549410fd8b2d36fe7098e318b1deb015d4b5
Philip-Murray <philip.murray@karhoo.com>
2020-07-02 09:49:08 +0200

    d7

--- Config/Config.go
+++ Config/Config.go
@@ -13,7 +13,7 @@ var (
 	Server_Publish_Topic         = "events.TripBooked"
 	Server_Pull_Topic            = "events.MakeReservation"
 	GRPC_PORT             string = ":3002"          //Exposed
-	Localhost_PubSub_PORT string = "localhost:8085" //Connecting to external
+	Localhost_PubSub_PORT string = "localhost:8432" //Connecting to external
 	PubSub_Project_Name          = "karhoo-local"
 )
 
