2525f01c52525f50323a76a132d595b22423fc3a
PHIL528 <philippaulmurray@gmail.com>
2020-07-03 16:07:49 +0200

    debugwatermill

--- Config/Config.go
+++ Config/Config.go
@@ -1,11 +1,9 @@
 package Config
 
 import (
+	"cloud.google.com/go/pubsub"
 	"context"
 	"errors"
-	"fmt"
-
-	"cloud.google.com/go/pubsub"
 )
 
 var (
@@ -13,30 +11,26 @@ var (
 	Server_Publish_Topic         = "events.TripBooked"
 	Server_Pull_Topic            = "events.MakeReservation"
 	GRPC_PORT             string = ":3002"          //Exposed
-	Localhost_PubSub_PORT string = "localhost:8432" //Connecting to external
+	Localhost_PubSub_PORT string = "localhost:8085" //Connecting to external
 	PubSub_Project_Name          = "karhoo-local"
 )
 
 func GetTopic(ctx context.Context, top string, admin bool) (*pubsub.Topic, context.Context, error) {
 	client, err := pubsub.NewClient(ctx, PubSub_Project_Name)
 	if err != nil {
-		fmt.Println("1")
 		return nil, nil, errors.New("Unable to setup connection with PubSub")
 	}
 	topic := client.Topic(top)
 	exists, err := topic.Exists(ctx)
 	if err != nil {
-		fmt.Println("2")
 		return nil, nil, errors.New("Could not check if topic exists")
 	} else if !exists {
 		if admin {
 			topic, err = client.CreateTopic(ctx, top)
 			if err != nil {
-				fmt.Println("3")
 				return nil, nil, errors.New("Could not create topic")
 			}
 		} else {
-			fmt.Println("4")
 			return nil, nil, errors.New("Topic has not been created yet")
 		}
 	}
